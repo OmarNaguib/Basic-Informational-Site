3816528f85dcfea48f68fffe79fc5c167237912a
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-08-20 15:50:02 +0300

    Add all pages

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const fs = require("fs/promises");
 
 const routeToFiles = {
   "/": "index.html",
+  "/about": "about.html",
+  "/contact-me": "contact-me.html",
 };
 
 const server = http.createServer(async (request, response) => {
   if (routeToFiles[request.url]) {
     response.writeHead(200, { "Content-Type": "text/html" });
-    const data = await fs.readFile(routeToFiles[request.url]);
-    await response.write(data);
+    const data = await fs.readFile(`pages/${routeToFiles[request.url]}`);
+    response.write(data);
   }
   response.end();
 });
