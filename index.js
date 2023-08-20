47608a5d950f2d9e2b9624c605473e74a03f63a9
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-08-20 15:40:50 +0300

    Make server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const port=8080
+const port = 8080;
+
+const http = require("http");
+const fs = require("fs/promises");
+
+const routeToFiles = {
+  "/": "index.html",
+};
+
+const server = http.createServer(async (request, response) => {
+  if (routeToFiles[request.url]) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    const data = await fs.readFile(routeToFiles[request.url]);
+    await response.write(data);
+  }
+  response.end();
+});
+
+server.listen(port);
